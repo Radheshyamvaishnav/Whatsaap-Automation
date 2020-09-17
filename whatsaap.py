357427de1b42d9e3f85288d55c8948d3e5586f28
Radheshyam <rdscode.py@gmail.com>
2020-09-17 23:08:03 +0530

    Whatsaap Automation

---
+++ whatsaap.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
+
+driver.get("https://web.whatsapp.com/")
+driver.maximize_window()
+
+name = input("Enter name of group or Individual")
+msg = input("Enter the msg")
+
+user = driver.find_element_by_xpath("//span[@title='{}']".format(name))
+user.click()
+msg_box = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+
+msg_box.send_keys(msg)
+
+driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
+
+print("Success")
+
